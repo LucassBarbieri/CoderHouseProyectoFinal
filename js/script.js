3a6e60334ae4578f0122e51b304ddc0c6604f65a
Lucas Barbieri <lucas.barbieri.f@gmail.com>
2022-06-24 16:04:55 -0300

    Re modelando sitio e imagenes

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function pic() {
     $('.headertopnav1').css({
         'background' : 'url('+ bgm[Math.floor(Math.random() * bgm.length)] + ') no-repeat',
         'background-position' : '100%',
-        'background-size' : 'cover'
+        'background-size' : 'cover',
     });
 }
 
